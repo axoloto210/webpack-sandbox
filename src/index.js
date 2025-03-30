6acfeef4a8c6911e145c3ac3a2b9fde0db11e3e4
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-03-30 16:18:28 +0900

    add print.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash'
 import './style.css';
 import Icon from './woop_org.png'
+import printMe from './print.js';
 
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button')
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -15,6 +17,11 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   
     return element;
   }
