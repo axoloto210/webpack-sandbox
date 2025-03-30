6f2c54e047b713a270694c789939a9085843bdb6
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-03-30 17:17:30 +0900

    add webpack-manifest-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {WebpackManifestPlugin} = require("webpack-manifest-plugin");
 
 module.exports = {
 	mode: "production",
@@ -11,6 +12,9 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			title: "Output Management",
 		}),
+        new WebpackManifestPlugin({
+            fileName: 'woop-manifest.json',
+          }),
 	],
 	output: {
 		filename: "[name].bundle.js",
