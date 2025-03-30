b6854fbbc91d53363b94ab69cb20dd2cfaf9befc
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-03-30 16:25:51 +0900

    add biome and format

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import _ from 'lodash'
-import './style.css';
-import Icon from './woop_org.png'
-import printMe from './print.js';
-
+import _ from "lodash";
+import "./style.css";
+import Icon from "./woop_org.png";
+import printMe from "./print.js";
 
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button')
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+	const element = document.createElement("div");
+	const btn = document.createElement("button");
 
-      // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+	// Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join(["Hello", "webpack"], " ");
+	element.classList.add("hello");
 
-  element.appendChild(myIcon);
+	// Add the image to our existing div.
+	const myIcon = new Image();
+	myIcon.src = Icon;
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+	element.appendChild(myIcon);
 
-  element.appendChild(btn);
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+	btn.innerHTML = "Click me and check the console!";
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
+
+	return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      //styleのロード設定
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      // imageのロード設定
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+	mode: "production",
+	entry: {
+		index: "./src/index.js",
+		print: "./src/print.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Output Management",
+		}),
+	],
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			//styleのロード設定
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			// imageのロード設定
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
 };
